7ede25f8ab23391fe064dca67a7c1b25b6215ef6
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-09-15 15:00:15 -0500

    Created basic map and added to assets folder

---
+++ assets/images/mapOne.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="mapOne" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
+ <image source="assetPack.png" width="384" height="384"/>
+</tileset>
