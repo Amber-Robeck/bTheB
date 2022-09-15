34446f2f591584c5d8a9e2601ad292a4ab311078
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-09-15 16:44:05 -0500

    Added background movement for appearance of player movement

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,6 +28,22 @@ class Sprite {
 };
 
 const background = new Sprite({ position: { x: -1745, y: -400 }, image: image })
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+};
+
 //animation loop
 function animate() {
     window.requestAnimationFrame(animate);
@@ -46,6 +62,19 @@ function animate() {
         100,
         100);
 
+    //background movement
+    if (keys.w.pressed) {
+        background.position.y += 3;
+    }
+    else if (keys.s.pressed) {
+        background.position.y -= 3;
+    }
+    else if (keys.a.pressed) {
+        background.position.x += 3;
+    }
+    else if (keys.d.pressed) {
+        background.position.x -= 3;
+    }
 };
 animate();
 
@@ -55,15 +84,37 @@ window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
             console.log('Pressed w/up')
+            keys.w.pressed = true;
+            // console.log('keys', keys)
             break;
         case 's':
             console.log('Pressed s/down')
+            keys.s.pressed = true;
             break;
         case 'a':
             console.log('Pressed a/left')
+            keys.a.pressed = true;
             break;
         case 'd':
             console.log('Pressed d/right')
+            keys.d.pressed = true;
+            break;
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false;
+            break;
+        case 's':
+            keys.s.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
             break;
     }
 });
