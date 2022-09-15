b92ff8c4733ee2abbe1c4a8c701ae2a17d298223
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-09-15 15:19:48 -0500

    Adjusted image sizes

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-ctx.fillStyle = 'lightgrey'
+ctx.fillStyle = 'lightgrey';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//draw image needs an html image object not src
+const image = new Image();
+image.src = './assets/images/mapOne.png';
+
+image.onload = () => {
+    //image, x start, y start
+    ctx.drawImage(image, 0, 0);
+};
 
